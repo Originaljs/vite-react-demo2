d97616ce0fb2c21918fb528bd352531d8c72bbd3
Originaljs <1154694433@qq.com>
2022-12-15 13:01:05 +0800

    20221215_tj_demo2_1

--- src/component/chlidren/RightInfos/index.tsx
+++ src/component/chlidren/RightInfos/index.tsx
@@ -2,10 +2,24 @@ import Style from './style.module.less'
 import { InfoTitle } from '../../InfoTitle'
 
 import hBg from '../../../assets/imgs/标题框.png'
+import { useRef, useEffect } from 'react'
 
 export const RightInfos = (props: any) => {
 
     const { list, errorList } = props
+
+    const errorListEl = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const tisk = () => {
+            requestAnimationFrame(tisk)
+            errorListEl.current!.scrollTop++
+            if (errorListEl.current!.scrollTop >= (errorListEl.current!.scrollHeight - errorListEl.current!.clientHeight) / 2 + errorListEl.current!.clientHeight / 2)
+                errorListEl.current!.scrollTop = 0
+        }
+
+        tisk()
+    }, [errorListEl])
     return (
         <div className={Style.rightInfo}>
             <div>
@@ -23,6 +37,11 @@ export const RightInfos = (props: any) => {
             </div>
             <div>
                 <InfoTitle bg={hBg} width={211} title='故障信息' />
+                <div className={Style.errorinfolist} ref={errorListEl}>
+                    {(errorList as Array<any>).map((item: any, index: number) => (
+                        <div key={item + index} className={Style.errorInfo}> {item}</div>
+                    ))}
+                </div>
             </div>
         </div>
     )
